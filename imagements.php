de24f17cab31809cb7df94bd9a2b425ba3a4c7b7
williewonka <williewonka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-08 20:57:12 +0000

    updating trunk to 1.3.0 alpha
    git-svn-id: https://plugins.svn.wordpress.org/imagements/trunk@622694 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- imagements.php
+++ imagements.php
@@ -2,8 +2,8 @@
 
 /*
 Plugin Name: imagements
-Description: this plugin lets your users put images in comments.
-Version: 1.2.1
+Description: lets your users put images in comments.
+Version: 1.3.0
 Author: williewonka
 Author URI: http://www.deweblogvanhelmond.nl
 License: GPL2
@@ -29,6 +29,7 @@ register_activation_hook(__file__, "imagements_init");
 register_deactivation_hook(__file__, 'imagements_deac');
 register_uninstall_hook(__file__, 'imagements_uninstall');
 add_action('wp_enqueue_scripts', 'imagement_add_form_tag');
+add_action('wp_enqueue_scripts', 'imagements_add_scripts');
 add_filter('comment_text', 'imagements_comment_check');
 add_action('comment_form_logged_in_after', 'imagements_additional_fields');
 add_action('comment_form_after_fields', 'imagements_additional_fields');
@@ -43,7 +44,7 @@ add_filter('pre_comment_content', 'imagements_edit_comment');
 
 require __dir__ . '/img_resize_function.php';
 require __dir__ . '/options.php';
-define("VERSION", "1.2.1");
+define("VERSION", "1.3.0");
 
 function imagements_version_check() //this function checks if the database is up to date with the latest format
 {
@@ -60,7 +61,7 @@ function imagements_version_check() //this function checks if the database is up
         hash text NOT NULL,
         UNIQUE KEY id (id)
         );";
-        
+
         require_once (ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($sql);
 
@@ -81,6 +82,10 @@ function imagements_version_check() //this function checks if the database is up
         update_option('tag', 'image');
         add_option('tag_use', 'user');
     }
+}
+
+function imagements_add_scripts(){
+    wp_enqueue_script('add_morefields_script', plugins_url('/js/more_fields.js', __file__));
 }
 
 function imagements_check_report_form_input() //this function checks if there has been input from the report button and if so handles it
@@ -113,8 +118,8 @@ function imagements_check_report_form_input() //this function checks if there ha
             if ($result == null)
             {
                 $sql = "INSERT INTO $table_name
-        VALUES (NULL, '" . $wpdb->prepare($_POST['image_name']) . "', '" . $wpdb->prepare($_POST['comment_id']) . "'
-        );";
+                VALUES (NULL, '" . $wpdb->prepare($_POST['image_name']) . "', '" . $wpdb->prepare($_POST['comment_id']) . "'
+                );";
                 $wpdb->query($sql);
                 wp_die(__('image reported, an admin will look at the case as soon as possible. Go <a href="javascript:history.back()">back</a> where you came from.'));
             } else
@@ -139,21 +144,25 @@ function imagements_deac() //this function clears some options when the plugin i
 
 function imagements_formverwerking() //this function handles the file uploading from the comment form
 {
-    if (isset($_POST['checkbox']))
+    $count = count($_FILES);
+    if ($count != 0)
     {
-        $name = $_FILES['image']['name'];
-        $hash = hash_file("md5", $_FILES['image']['tmp_name']);
-        move_uploaded_file($_FILES["image"]["tmp_name"], __dir__ . '/images/' . $name);
-        if (!imagements_resize_image(__dir__ . '/images/' . $name, 0, 1, get_option('max_width'), get_option('max_height')))
+        for ($i = 0; $i < $count; $i++)
         {
-            wp_die(__('error in resizing image, hit the back button and try again. <br> if problem persist contact site admin'));
+            $name = $_FILES['image']['name'][$i];
+            $hash = hash_file("md5", $_FILES['image']['tmp_name'][$i]);
+            move_uploaded_file($_FILES["image"]["tmp_name"][$i], __dir__ . '/images/' . $name);
+            if (!imagements_resize_image(__dir__ . '/images/' . $name, 0, 1, get_option('max_width'), get_option('max_height')))
+            {
+                wp_die(__('error in resizing image, hit the back button and try again. <br> if problem persist contact site admin'));
+            }
+            global $wpdb;
+            $table_name = $wpdb->prefix . 'imagements';
+            $sql = "INSERT INTO $table_name
+            VALUES (NULL, '" . $wpdb->prepare($_POST['naam'][$i]) . "', '" . $wpdb->prepare($_FILES['image']['name'][$i]) . "', 'no', '$hash') 
+            ";
+            $wpdb->query($sql);
         }
-        global $wpdb;
-        $table_name = $wpdb->prefix . 'imagements';
-        $sql = "INSERT INTO $table_name
-        VALUES (NULL, '" . $wpdb->prepare($_POST['naam']) . "', '" . $wpdb->prepare($_FILES['image']['name']) . "', 'no', '$hash') 
-        ";
-        $wpdb->query($sql);
 
     }
 }
@@ -162,10 +171,13 @@ function imagements_edit_comment($comment) //this function adds a tag automaticl
 {
     $option = get_option('tag_use');
     $tag = get_option('tag');
+    $count = count($_FILES);
 
-    if ($option == 'auto' && isset($_POST['checkbox']))
+    if ($option == 'auto' && $count != 0)
     {
-        $comment = $comment . '<br><br>[' . $tag . '=' . $_POST['naam'] . ']';
+        for($i=0;$i<$count;$i++){
+            $comment = $comment . '<br>[' . $tag . '=' . $_POST['naam'][$i] . ']';
+        }
     }
 
     return $comment;
@@ -175,68 +187,71 @@ function imagements_edit_comment($comment) //this function adds a tag automaticl
 function imagements_verify_post_data($commentdata) //this function checks if the input in the comment form is valid for this plugin
 {
     $option = get_option('tag_use');
-
-    if (isset($_POST["checkbox"]))
+    $count_images = count($_FILES);
+    if ($count_images != 0)
     {
-        if ($_FILES['image']['name'] == null)
+        for ($i = 0; $i < $count_images; $i++)
         {
-            wp_die(__('no file was specified. Hit the Back button on your Web browser and try agian'));
-        } else
-        {
-            if ($_POST['naam'] == null && $option == 'user')
+            if ($_FILES['image']['name'][$i] == null)
             {
-                wp_die(__('no name was specified. Hit the Back button and try agian.'));
+                wp_die(__('no file was specified. Hit the Back button on your Web browser and try agian'));
             } else
             {
-                if ($option == 'auto')
+                if ($_POST['naam'][$i] == null && $option == 'user')
                 {
-                    $_POST['naam'] = $_FILES['image']['name'];
-                }
-                if ($_FILES["file"]["error"] > 0)
-                {
-                    wp_die(__('file Error: ' . $_FILES['image']['error'] . ' Hit the Back button on your Web browser and try again'));
+                    wp_die(__('no name was specified. Hit the Back button and try agian.'));
                 } else
                 {
-                    if (!($_FILES['image']['type'] == 'image/x-png' || $_FILES['image']['type'] == 'image/pjpeg' || $_FILES['image']['type'] == 'image/jpeg' || $_FILES['image']['type'] == 'image/jpg' || $_FILES['image']['type'] == 'image/png'))
+                    if ($option == 'auto')
                     {
-                        wp_die(__('this file is no image. Hit the Back button on your Web browser and try again'));
+                        $_POST['naam'][$i] = $_FILES['image']['name'][$i];
+                    }
+                    if ($_FILES["file"]["error"][$i] > 0)
+                    {
+                        wp_die(__('file Error: ' . $_FILES['image']['error'][$i] . ' Hit the Back button on your Web browser and try again'));
                     } else
                     {
-                        global $wpdb;
-                        $table_name = $wpdb->prefix . "imagements";
-
-                        $sql = "SELECT naam
-                        FROM $table_name
-                        WHERE naam = '" . $wpdb->prepare($_POST['naam']) . "'
-                        ";
-                        $result = $wpdb->get_var($sql);
-                        if ($result != null)
+                        if (!($_FILES['image']['type'][$i] == 'image/x-png' || $_FILES['image']['type'][$i] == 'image/pjpeg' || $_FILES['image']['type'][$i] == 'image/jpeg' || $_FILES['image']['type'][$i] == 'image/jpg' || $_FILES['image']['type'][$i] == 'image/png'))
                         {
-                            wp_die(__('this name is already in use. Hit the Back button and choose a new unique one'));
+                            wp_die(__('this file is no image. Hit the Back button on your Web browser and try again'));
                         } else
                         {
-                            $dir = plugin_dir_url(__file__) . '/images/';
-                            if (file_exists($dir . $_FILES["image"]["name"]))
+                            global $wpdb;
+                            $table_name = $wpdb->prefix . "imagements";
+
+                            $sql = "SELECT naam
+                            FROM $table_name
+                            WHERE naam = '" . $wpdb->prepare($_POST['naam'][$i]) . "'
+                            ";
+                            $result = $wpdb->get_var($sql);
+                            if ($result != null)
                             {
-                                wp_die(__('this filename already exists on our server. please rename the file and try againg'));
+                                wp_die(__('this name is already in use. Hit the Back button and choose a new unique one'));
                             } else
                             {
-                                $sql = " SELECT hash
-                                FROM $table_name
-                                ";
-                                $result = $wpdb->get_col($sql);
-                                $count = count($result);
-                                $hash = hash_file("md5", $_FILES["image"]["tmp_name"]);
-                                for ($i = 0; $i < $count; $i++)
+                                $dir = plugin_dir_url(__file__) . '/images/';
+                                if (file_exists($dir . $_FILES["image"]["name"][$i]))
                                 {
-                                    if ($result[$i] == $hash)
+                                    wp_die(__('this filename already exists on our server. please rename the file and try againg'));
+                                } else
+                                {
+                                    $sql = " SELECT hash
+                                    FROM $table_name
+                                    ";
+                                    $result = $wpdb->get_col($sql);
+                                    $count = count($result);
+                                    $hash = hash_file("md5", $_FILES["image"]["tmp_name"][$i]);
+                                    for ($i = 0; $i < $count; $i++)
                                     {
-                                        wp_die(__('This file already exists on our server.'));
+                                        if ($result[$i] == $hash)
+                                        {
+                                            wp_die(__('This file already exists on our server.'));
+                                        }
                                     }
                                 }
                             }
-                        }
 
+                        }
                     }
                 }
             }
@@ -248,12 +263,13 @@ function imagements_verify_post_data($commentdata) //this function checks if the
 function imagements_additional_fields() //this function adds fields to the comment form
 {
     $option = get_option('tag_use');
-    echo '<p>' . '<label for="checkbox">' . __('upload image') . '</label>' . '<input id="checkbox" name="checkbox" type="checkbox" value ="yes"/></p>';
     if ($option == 'user')
     {
-        echo '<p>' . '<label for="naam">' . __('name image') . '</label>' . '<input id="naam" name="naam" type="text"/></p>';
+        $fields = '<p>' . '<label for="naam">' . __('name image') . '</label>' . '<input id="naam" name="naam[]" type="text"/></p>';
     }
-    echo '<p>' . '<label for="image">' . __('file image') . '</label>' . '<input id="image" name="image" type="file"/></p>';
+    $fields = $fields . '<p>' . '<label for="image">' . __('file image') . '</label>' . '<input id="image" name="image[]" type="file"/></p>';
+    echo $fields;
+    echo "<p><input type='button' id='extra_field' value='extra image upload field' onClick='extend($fields)'></p>";
 }
 
 function imagements_comment_check() //this function check every comment to see if an image has to be inserted and if so handles this
---
+++ js/more_fields.js
@@ -0,0 +1,5 @@
+function extend(fields)
+{
+var formholder = fields
+document.getElementById('commentform').innerHTML = document.getElementById('commentform').innerHTML + formholder;
+}
